444e01335c681d40525dc5a7eeea3bdcd270911a
SunnySunhwa <via1229@gmail.com>
2018-07-30 23:52:00 +1200

    implementation for user login

--- JWT-example/routes/api/auth/auth.controller.js
+++ JWT-example/routes/api/auth/auth.controller.js
@@ -1,5 +1,5 @@
 const User = require('../../../models/user')
-
+const jwt = require('jsonwebtoken')
 /*
     POST /api/auth
     {
@@ -59,4 +59,73 @@ exports.register = (req, res) => {
     .then(assign)
     .then(respond)
     .catch(onError)
+}
+
+/*
+    POST /api/auth/login
+    {
+        username,
+        password
+    }
+*/
+
+exports.login = (req, res) => {
+  const {username, password} = req.body
+  const secret = req.app.get('jwt-secret')
+
+  // check the user info & generate the jwt
+      // check the user info & generate the jwt
+  const check = (user) => {
+      if(!user) {
+          // user does not exist
+          throw new Error('login failed')
+      } else {
+          // user exists, check the password
+          if(user.verify(password)) {
+              // create a promise that generates jwt asynchronously
+              const p = new Promise((resolve, reject) => {
+                  jwt.sign(
+                      {
+                          _id: user._id,
+                          username: user.username,
+                          admin: user.admin
+                      }, 
+                      secret, 
+                      {
+                          expiresIn: '7d',
+                          issuer: 'velopert.com',
+                          subject: 'userInfo'
+                      }, (err, token) => {
+                          if (err) reject(err)
+                          resolve(token) 
+                      })
+              })
+              return p
+          } else {
+              throw new Error('login failed')
+          }
+      }
+  }
+
+  // respond the token 
+  const respond = (token) => {
+      res.json({
+          message: 'logged in successfully',
+          token
+      })
+  }
+
+  // error occured
+  const onError = (error) => {
+      res.status(403).json({
+          message: error.message
+      })
+  }
+
+  // find the user
+  User.findOneByUsername(username)
+  .then(check)
+  .then(respond)
+  .catch(onError)
+
 }
