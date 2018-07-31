0861e7cf7720adc1b357e84b23b547823017641c
SunnySunhwa <via1229@gmail.com>
2018-07-31 16:36:49 +1200

    implementation for checking JWT

--- JWT-example/routes/api/auth/index.js
+++ JWT-example/routes/api/auth/index.js
@@ -3,5 +3,7 @@ const controller = require('./auth.controller')
 
 router.post('/register', controller.register)
 router.post('/login', controller.login)
+router.get('/check', controller.check)
+
 
 module.exports = router
