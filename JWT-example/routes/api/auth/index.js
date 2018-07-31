28a136daf3a31a18bea0974cda2e4c7ad1219eaa
SunnySunhwa <via1229@gmail.com>
2018-07-31 16:46:20 +1200

    use middlewares for checking jwt

--- JWT-example/routes/api/auth/index.js
+++ JWT-example/routes/api/auth/index.js
@@ -3,6 +3,8 @@ const controller = require('./auth.controller')
 
 router.post('/register', controller.register)
 router.post('/login', controller.login)
+
+router.use('/check', authMiddleware)
 router.get('/check', controller.check)
 
 
