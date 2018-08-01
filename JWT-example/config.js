daeb85f9094d9b8abfd1a8588a7a961e22ee9d48
SunnySunhwa <via1229@gmail.com>
2018-08-01 21:17:57 +1200

    fixed untracked files

--- JWT-example/config.js
+++
@@ -1,6 +0,0 @@
-// It has Secret key. in real project, should be in .gitignore
-module.exports = {
-
-  'secret': 'SeCrEtKeYfOrHaShInG',
-  'mongodbUri': 'mongodb://user:password0@ds127492.mlab.com:27492/jwt_prac'
-}
